fb8d0f7d73493b33112b4425c83d184d2767f662
ChoiDeborah <adorablejimini@gmail.com>
2022-05-05 10:49:54 +0900

    3-1 연산자 우선순위와 결합 법칙

---
+++ Chapter3/3-1 연산자 우선순위와 결합 법칙.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	//int x = 4 + 2 * 3; // add(4, mult(2, 3))
+	
+	int x = 3 * 4 / 2;	// 연산자들의 우선순위가 같은 경우 결합 법칙 (Associativity)
+						// 우선 순위
+						// https://ko.wikipedia.org/wiki/C%EC%99%80_C%2B%2B%EC%9D%98_%EC%97%B0%EC%82%B0%EC%9E%90
+
+	
+	// Unary 의 경우 왼쪽 인자가 없기 때문에 Right-to-left
+	//ex) -3 ==> +(-3)
+	
+	// r = 1 + 2 + 3 * 4
+	// A = B = C;
+	// t/= --w + 5
+	// a||b && c||d
+
+
+	return 0;
+}
